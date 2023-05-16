f93d2fa3a9a4e42c0b9a87a2e54cce43c3659cf3
1-Shreyash <108738197+1-Shreyash@users.noreply.github.com>
2023-05-16 22:48:53 +0530

    changes💫

---
+++ 16May/Employee.java
@@ -0,0 +1,5 @@
+public class Employee {
+    String UserId = new String();
+    String Password = new String();
+    
+}
---
+++ 16May/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+// Create a function to take input from user 10 positive numbers. If the number entered is less than 0 throw Arithemetic eception. The exception should be handled in main() class. 
+// Create a class Employee which stores UserID and Password. The password is valid if it contains a UpperCase letter, a LowerCase letter, a number and a special character (@, $, &, *) and length should be a minimum of 8 characters. If the password is not valid then throw user defined exception InvalidPasswordException.
+class Main{
+    public static void main(String[] args) {
+        
+    }
+}
